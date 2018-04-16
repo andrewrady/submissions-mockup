08519574dcf1dacff02229d5ae8199cbc3859507
Andrew Rady <andrew.rady@ritterim.com>
2018-04-16 13:31:06 -0600

    updated fields

--- src/components/KeyEntry.vue
+++ src/components/KeyEntry.vue
@@ -37,7 +37,12 @@
 
           <div class="field" v-if="step == 7">
             <label>Card Type</label>
-            <input type="text" v-model="form.billingInformation.cardType" @keydown="nextField">
+            <select v-model="form.billingInformation.cardType" @change="nextField">
+              <option value="Visa">Visa</option>
+              <option value="Visa">American Express</option>
+              <option value="Visa">Discover</option>
+              <option value="Visa">Master Card</option>
+            </select>
           </div>
           <div class="field" v-if="step == 8">
             <label>Card Number</label>
@@ -49,7 +54,21 @@
           </div>
           <div class="field" v-if="step == 10">
             <label>Experation Month</label>
-            <input type="text" v-model="form.billingInformation.experationMonth" @keydown="nextField">
+            <select class="ui fluid search dropdown" name="card[expire-month]" v-model="form.billingInformation.experationMonth" @change="nextField">
+                  <option value="">Month</option>
+                  <option value="1">January</option>
+                  <option value="2">February</option>
+                  <option value="3">March</option>
+                  <option value="4">April</option>
+                  <option value="5">May</option>
+                  <option value="6">June</option>
+                  <option value="7">July</option>
+                  <option value="8">August</option>
+                  <option value="9">September</option>
+                  <option value="10">October</option>
+                  <option value="11">November</option>
+                  <option value="12">December</option>
+                </select>
           </div>
           <div class="field" v-if="step == 11">
             <label>Experation Year</label>
